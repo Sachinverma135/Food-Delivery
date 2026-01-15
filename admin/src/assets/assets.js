3fd261e19932255a55b1d0cfca0acd92880ffe84
Sachinverma135 <sachinsssty123@gmail.com>
2026-01-15 21:51:51 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,6 +4,7 @@ import add_icon_white from './add_icon_white.png'
 import order_icon from './order_icon.png'
 import Upload_icon from './Upload_icon.png'
 
+export const url = "https://food-del-backend-q0w3.onrender.com"
 export const assets = {
     logo,
     admin_profile,
