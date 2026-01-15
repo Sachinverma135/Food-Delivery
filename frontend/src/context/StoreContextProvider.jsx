6e0653e548282cb9949a2738d70f7303cc496a38
Sachinverma135 <sachinsssty123@gmail.com>
2026-01-15 21:49:48 +0530

    Update StoreContextProvider.jsx

--- frontend/src/context/StoreContextProvider.jsx
+++ frontend/src/context/StoreContextProvider.jsx
@@ -5,7 +5,7 @@ import { StoreContext } from "./Storecontext";
 
 const StoreContextProvider = (props) => {
   const [cartitems, setCartitems] = useState({});
-  const url="http://localhost:4000"
+  const url="https://food-del-backend-q0w3.onrender.com"
 
   const [token,setToken] = useState()
      
